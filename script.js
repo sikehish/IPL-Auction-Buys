cbff269a5e445dd1189b0523b364e52c196374c7
Hisham Akmal <hisham0502@gmail.com>
2024-04-16 13:21:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
 "use strict";
 
 const players = JSON.parse(localStorage.getItem("players")) || {};
+let budget = localStorage.getItem("budget")
 
+while(budget===null || isNaN(budget) || budget===""){
+    budget=prompt("Enter a valid budget in Cr")
+}
+budget=Number(budget)
+localStorage.setItem("budget",budget)
+
+const statsContainer = document.getElementById("stats-container");
 const playersContainer = document.getElementById("players-container");
+
+const  budgetElement= document.createElement("div");
+budgetElement.textContent = `Budget: ${budget} Cr`;
+budgetElement.className="budget"
+statsContainer.appendChild(budgetElement);
+
 
 for (const playerName in players) {
     const player = players[playerName];
     const playerElement = document.createElement("div");
     playerElement.textContent = `${playerName}: ${player.price} ${player.denomination}`;
+    playerElement.className="player"
     playersContainer.appendChild(playerElement);
 }
 
+const addPlayer = (playerName, player) =>{
+    addPlayerElement(playerName, player)
+    budget-=player.price
+    budgetElement.textContent = `Budget: ${budget} Cr`;
+    localStorage.setItem("budget",budget)
+}
+
 const addPlayerElement = (playerName, player) =>{
-    const playersContainer = document.getElementById("players-container");
     const playerElement = document.createElement("div");
     playerElement.textContent = `${playerName}: ${player.price} ${player.denomination}`;
     playersContainer.appendChild(playerElement);
@@ -40,6 +61,11 @@ document.getElementById("player-form").addEventListener("submit", (e) => {
         return;
     }
 
+    if(playerPrice>=budget){
+        alert("Player price exceeds the budget!");
+        return;
+    }
+
     players[playerName] = { price: playerPrice, denomination: denomination };
     localStorage.setItem("players", JSON.stringify(players));
 
@@ -47,7 +73,7 @@ document.getElementById("player-form").addEventListener("submit", (e) => {
     document.getElementById("player-price-input").value = "";
     document.getElementById("denomination").value = "lakhs";
 
-    addPlayerElement(playerName, players[playerName])
+    addPlayer(playerName, players[playerName])
 });
 
 document.getElementById("delete-all-btn").addEventListener("click", (e) => {
