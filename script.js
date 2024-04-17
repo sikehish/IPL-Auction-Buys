9891bd420e38ddc860214d0426a8189d633daa0d
Hisham Akmal <hisham0502@gmail.com>
2024-04-17 13:23:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,97 @@
 "use strict";
 
-const players = JSON.parse(localStorage.getItem("players")) || {};
-let budget = localStorage.getItem("budget")
+let budget = localStorage.getItem("budget");
 
-while(budget===null || isNaN(budget) || budget===""){
-    budget=prompt("Enter a valid budget in Cr")
+function formatBudget(budget) {
+    let rupee = new Intl.NumberFormat("en-IN", {
+        style: "currency",
+        currency: "INR",
+    });
+
+    if (budget >= 10000000) return `Rs ${rupee.format(budget / 10000000)} Cr`;
+    if (budget >= 100000) return `Rs ${rupee.format(budget / 100000)} Lakh`;
+    return `Rs ${budget}`;
 }
-budget=Number(budget)
-localStorage.setItem("budget",budget)
+
+
+const inputBudgetIfEmpty=(operation)=>{
+    while (budget === null || isNaN(budget) || budget === "") {
+        budget = prompt("Enter a valid budget in Cr");
+        if(operation=="delete-all" && budget===null) return "cancel"
+        if (Number(budget)) {
+            budget *= 10000000;
+            localStorage.setItem("originalBudget",budget);
+            console.log(budget)
+        }
+    }
+    budget = Number(budget);
+    localStorage.setItem("budget", budget);
+}
+
+inputBudgetIfEmpty("first-render")
 
 const statsContainer = document.getElementById("stats-container");
 const playersContainer = document.getElementById("players-container");
 
-const  budgetElement= document.createElement("div");
-budgetElement.textContent = `Budget: ${budget} Cr`;
-budgetElement.className="budget"
+const budgetElement = document.createElement("div");
+budgetElement.textContent = `Budget: ${formatBudget(budget)}`;
+budgetElement.className = "budget";
 statsContainer.appendChild(budgetElement);
 
+const players = JSON.parse(localStorage.getItem("players")) || {};
 
 for (const playerName in players) {
     const player = players[playerName];
     const playerElement = document.createElement("div");
     playerElement.textContent = `${playerName}: ${player.price} ${player.denomination}`;
-    playerElement.className="player"
+    playerElement.className = "player";
     playersContainer.appendChild(playerElement);
 }
 
-const addPlayer = (playerName, player) =>{
-    addPlayerElement(playerName, player)
-    budget-=player.price
-    budgetElement.textContent = `Budget: ${budget} Cr`;
-    localStorage.setItem("budget",budget)
+function manageBudget(player, operation) {
+    const playerPrice =
+        player.price * (player.denomination === "crore"? 10000000 : 100000);
+    switch (operation) {
+        case "add":
+            if (playerPrice >= budget) {
+                throw new Error("Player price cannot exceed budget!");
+            }
+            budget -= playerPrice;
+            break;
+        case "remove":
+            budget += playerPrice;
+            break;
+    }
 }
 
-const addPlayerElement = (playerName, player) =>{
+const addPlayer = (playerName, player) => {
+    try {
+        manageBudget(player, "add");
+        budgetElement.textContent = `Budget: ${formatBudget(budget)}`;
+        localStorage.setItem("budget", budget);
+        players[playerName] = player;
+        localStorage.setItem("players", JSON.stringify(players));
+        addPlayerElement(playerName, player);
+        console.log(players)
+    } catch (error) {
+        throw new Error(e.message)
+    }
+};                       
+
+const addPlayerElement = (playerName, player) => {
     const playerElement = document.createElement("div");
     playerElement.textContent = `${playerName}: ${player.price} ${player.denomination}`;
     playersContainer.appendChild(playerElement);
-}
+};
 
 document.getElementById("player-form").addEventListener("submit", (e) => {
     e.preventDefault();
     const players = JSON.parse(localStorage.getItem("players")) || {};
-    const playerName = document.getElementById("player-name-input").value.trim();
-    const playerPrice = Number(document.getElementById("player-price-input").value.trim());
+    const playerName = document.getElementById("player-name-input")
+       .value.trim();
+    const playerPrice = Number(
+        document.getElementById("player-price-input").value.trim()
+    );
     const denomination = document.getElementById("denomination").value;
 
     if (!playerName) {
@@ -56,27 +104,33 @@ document.getElementById("player-form").addEventListener("submit", (e) => {
         return;
     }
 
-    if(playerName in players){
+    if (playerName in players) {
         alert("Player already entered!");
         return;
     }
 
-    if(playerPrice>=budget){
-        alert("Player price exceeds the budget!");
+    const player = { price: playerPrice, denomination };
+
+    try {
+        addPlayer(playerName, player);
+    } catch (e) {
+        alert(e.message);
         return;
     }
-
-    players[playerName] = { price: playerPrice, denomination: denomination };
-    localStorage.setItem("players", JSON.stringify(players));
 
     document.getElementById("player-name-input").value = "";
     document.getElementById("player-price-input").value = "";
     document.getElementById("denomination").value = "lakhs";
-
-    addPlayer(playerName, players[playerName])
 });
 
 document.getElementById("delete-all-btn").addEventListener("click", (e) => {
-    localStorage.clear()
-    playersContainer.innerHTML=""
-})
+    budget=null
+    if(inputBudgetIfEmpty("delete-all")=="cancel") {
+        console.log("INSIDE")
+        budget=localStorage.getItem("budget")
+        return
+    }
+    localStorage.removeItem("players")
+    budgetElement.textContent = `Budget: ${formatBudget(budget)}`;
+    playersContainer.innerHTML = "";
+});
